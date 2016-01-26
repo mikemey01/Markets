a87677a13a94496a39718915ce58e186355b58f5
mikemey01 <mikemey01@gmail.com>
2016-01-25 20:51:11 -0700

    setting up MarketDay

--- app/src/main/java/com/chariotinstruments/markets/MarketDay.java
+++ app/src/main/java/com/chariotinstruments/markets/MarketDay.java
@@ -2,7 +2,9 @@ package com.chariotinstruments.markets;
 
 import android.text.format.DateFormat;
 
+import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -11,8 +13,25 @@ import java.util.Date;
 public class MarketDay {
 
     private DateFormat _marketDate;
+    private ArrayList<MarketMinute> _marketMinutes;
 
     public MarketDay(){
-        
+        _marketMinutes = new ArrayList<MarketMinute>;
     }
+
+    private void addMinute(int curMinute, BigDecimal open, BigDecimal high, BigDecimal low, BigDecimal close, Long volume){
+
+        MarketMinute minute = new MarketMinute();
+
+        minute.setMinute(curMinute);
+        minute.setOpen(open);
+        minute.setHigh(high);
+        minute.setLow(low);
+        minute.setClose(close);
+        minute.setVolume(volume);
+
+        this._marketMinutes.add(minute);
+    }
+
+
 }
