12c92d2c558709d0bb545f3b133d918025924836
mikemey01 <mikemey01@gmail.com>
2016-01-25 21:24:57 -0700

    adding enumeration through arraylist

--- app/src/main/java/com/chariotinstruments/markets/ParseData.java
+++ app/src/main/java/com/chariotinstruments/markets/ParseData.java
@@ -6,11 +6,14 @@ import java.util.ArrayList;
  * Created by user on 1/25/16.
  */
 public class ParseData {
+
+    private String _target;
     private ArrayList<String> _data;
 
     //Constructor
     public ParseData(){
         _data = new ArrayList<String>();
+        _target = "1,";
     }
 
     //getters/setters
@@ -20,5 +23,29 @@ public class ParseData {
 
     public void setData(ArrayList<String> dataIn){
         _data = dataIn;
+    }
+
+    //add all of the current minutes from Google for the ticker to the MarketDay Object
+    public MarketDay populateMarketDay(){
+        MarketDay marketDay = new MarketDay();
+
+        return marketDay;
+    }
+
+    //trim the total ArrayList of strings down to just market data.
+    private ArrayList<String> trimData(ArrayList<String> dataIn){
+        ArrayList<String> trimmedData = new ArrayList<String>();
+        int count = 0;
+        int targetIndex = 0;
+
+        for (String item : trimmedData){
+            if(item.contains(_target)){
+                targetIndex = count;
+                break;
+            }
+            count++;
+        }
+
+        return trimmedData;
     }
 }
