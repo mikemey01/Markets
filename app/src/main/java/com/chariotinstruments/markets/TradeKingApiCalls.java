335e274e73fb9a0c7b7b3f34f2f5432252ec6fe7
mikemey01 <mikemey01@gmail.com>
2016-02-08 20:40:41 -0700

    adding api calls

--- app/src/main/java/com/chariotinstruments/markets/TradeKingApiCalls.java
+++ app/src/main/java/com/chariotinstruments/markets/TradeKingApiCalls.java
@@ -7,20 +7,37 @@ public class TradeKingApiCalls {
 
     private static final String RESPONSE_TYPE = ".xml";
 
-    private static final String PROFILE_URL = "https://api.tradeking.com/v1/member/profile"+RESPONSE_TYPE;
-    private static final String TICKER_URL = "https://api.tradeking.com/v1/market/options/search"+RESPONSE_TYPE+"?symbol=";
-    private static final String MARKET_CLOCK = "https://api.tradeking.com/v1/market/clock"+RESPONSE_TYPE;
-    private static final String MARKET_QUOTE = "https://api.tradeking.com/v1/market/ext/quotes"+RESPONSE_TYPE+"?symbols=";
-
     public TradeKingApiCalls(){
 
     }
+
+    //region accounts
+
+    private static final String PROFILE_URL = "https://api.tradeking.com/v1/member/profile"+RESPONSE_TYPE;
+    private static final String ACCOUNT_INFO_URL = "https://api.tradeking.com/v1/accounts"+RESPONSE_TYPE;
 
     public String getProfile(){
         return PROFILE_URL;
     }
 
-    public String getTicker(String symbol){
+    public String getFullAccountInfo() {
+        return ACCOUNT_INFO_URL;
+    }
+
+    //endregion
+
+    //region orders
+
+    //endregion
+
+    //region market calls
+
+    private static final String TICKER_URL = "https://api.tradeking.com/v1/market/options/search"+RESPONSE_TYPE+"?symbol=";
+    private static final String MARKET_CLOCK = "https://api.tradeking.com/v1/market/clock"+RESPONSE_TYPE;
+    private static final String MARKET_QUOTE = "https://api.tradeking.com/v1/market/ext/quotes"+RESPONSE_TYPE+"?symbols=";
+    private static final String MARKET_HISTORICAL_QUOTE = "https://api.tradeking.com/v1/market/timesales"+RESPONSE_TYPE+"?symbols=";
+
+    public String getTickerCurrent(String symbol){
         return TICKER_URL+symbol;
     }
 
@@ -31,4 +48,14 @@ public class TradeKingApiCalls {
     public String getMarketQuote(String symbol){
         return MARKET_QUOTE+symbol;
     }
+
+    public String getMarketHistoricalQuote(String symbol){
+        return MARKET_HISTORICAL_QUOTE+symbol+"&startdate=2016-02-08&interval=1min";
+    }
+
+    //endregion
+
+
+
+
 }
