cebd023c0a02a49d333abd45f8ab6675d6d5a9e1
mikemey01 <mikemey01@gmail.com>
2016-02-17 21:24:28 -0700

    adding parse account data class

---
+++ app/src/main/java/com/chariotinstruments/markets/ParseAccountData.java
@@ -0,0 +1,22 @@
+package com.chariotinstruments.markets;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by user on 2/17/16.
+ */
+public class ParseAccountData extends AsyncTask<Void, Void, String> {
+
+    protected void onPreExecute(){
+
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        return null;
+    }
+
+    protected void onPostExecute(String result){
+        
+    }
+}
