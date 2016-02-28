fb188c3abe6693eee2363d987da44ca7f49b2d86
mikemey01 <mikemey01@gmail.com>
2016-02-28 16:49:16 -0700

    starting phase one process flow

---
+++ app/src/main/java/com/chariotinstruments/markets/PhaseOneControl.java
@@ -0,0 +1,31 @@
+package com.chariotinstruments.markets;
+
+/**
+ * Created by user on 2/28/16.
+ */
+public class PhaseOneControl {
+
+    public PhaseOneControl(){
+
+    }
+
+    public void start(){
+
+    }
+
+    private void dataRetrievalLoop(){
+
+    }
+
+    private void checkIndicators(){
+
+    }
+
+    private void submitOrder(){
+        
+    }
+
+    private void stop(){
+
+    }
+}
