71ddc73323e6f2656c8d54187d56dbdd256b5563
mikemey01 <mikemey01@gmail.com>
2016-03-02 18:06:12 -0700

    adding stock quote classes

--- app/src/main/java/com/chariotinstruments/markets/MarketQuote.java
+++
@@ -1,7 +0,0 @@
-package com.chariotinstruments.markets;
-
-/**
- * Created by user on 3/1/16.
- */
-public class MarketQuote {
-}
---
+++ app/src/main/java/com/chariotinstruments/markets/StockQuote.java
@@ -0,0 +1,7 @@
+package com.chariotinstruments.markets;
+
+/**
+ * Created by user on 3/2/16.
+ */
+public class StockQuote {
+}
