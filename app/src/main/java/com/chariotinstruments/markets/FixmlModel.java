f2ef4a2071ee00d74d65d9f0dd0e3c49577137f2
mikemey01 <mikemey01@gmail.com>
2016-03-12 14:15:45 -0700

    building out trade control

--- app/src/main/java/com/chariotinstruments/markets/FixmlModel.java
+++ app/src/main/java/com/chariotinstruments/markets/FixmlModel.java
@@ -12,7 +12,7 @@ public class FixmlModel {
     private int orderType; // "Typ" Price Type as "1" ‐ Market, "2" ‐ Limit", "3" ‐ Stop, "4" Stop Limit, or "P" for trailing stop.
     private int orderSide; // "Side" Side of market as "1" ‐ Buy, "2" ‐ Sell, "5" ‐ Sell Short. Buy to cover orders are attributed as buy orders with Side="1".
     private String positionEffect; // Used for options, option legs require and attribute of "O" for opening or "C" for closing.
-    private String stockOrOption; //"CFI" "classification of financial instrument", used for options to distinguish "OC" for call option or "OP" for put option.
+    private String CFI; //"CFI" "classification of financial instrument", used for options to distinguish "OC" for call option or "OP" for put option.
     private String secType; // "secTyp" Security type attribute is needed. "CS" for common stock or "OPT" for option.
     private String expDate; // "MatDt" Represents the expiration date of a option. Needs to be in the format of "YYYY‐MM‐ DDT00:00:00.000‐05:00".
     private double strikePrice; // "strkPx" Strike price of option contract
@@ -23,12 +23,12 @@ public class FixmlModel {
         isOrderLive = isLive;
     }
 
-    public FixmlModel(boolean isLive, int orderType, int orderSide, String positionEffect, String stockOrOption, String secType, String expDate, double strikePrice, String symbol, int quantity){
+    public FixmlModel(boolean isLive, int orderType, int orderSide, String positionEffect, String CFI, String secType, String expDate, double strikePrice, String symbol, int quantity){
         this.isOrderLive = isLive;
         this.orderType = orderType;
         this.orderSide = orderSide;
         this.positionEffect = positionEffect;
-        this.stockOrOption = stockOrOption;
+        this.CFI = CFI;
         this.secType = secType;
         this.expDate = expDate;
         this.strikePrice = strikePrice;
@@ -36,12 +36,12 @@ public class FixmlModel {
         this.quantity = quantity;
     }
 
-    public void createFIXMLObject(boolean isLive, int orderType, int orderSide, String positionEffect, String stockOrOption, String secType, String expDate, double strikePrice, String symbol, int quantity){
+    public void createFIXMLObject(boolean isLive, int orderType, int orderSide, String positionEffect, String CFI, String secType, String expDate, double strikePrice, String symbol, int quantity){
         this.isOrderLive = isLive;
         this.orderType = orderType;
         this.orderSide = orderSide;
         this.positionEffect = positionEffect;
-        this.stockOrOption = stockOrOption;
+        this.CFI = CFI;
         this.secType = secType;
         this.expDate = expDate;
         this.strikePrice = strikePrice;
@@ -54,7 +54,7 @@ public class FixmlModel {
 
         output = "<FIXML xmlns=\"http://www.fixprotocol.org/FIXML-5-0-SP2\">";
         output = output + "<Order TmInForce=\"0\" Typ=\""+orderType+"\" Side=\""+orderSide+"\" PosEfct=\""+positionEffect+"\" Acct=\""+apiKeys.ACCOUNT_NUMBER+"\">";
-        output = output + "<Instrmt CFI=\""+stockOrOption+"\" SecTyp=\""+secType+"\" MatDt=\""+expDate+"\" StrkPx=\""+strikePrice+"\" Sym=\""+symbol+"\"/>";
+        output = output + "<Instrmt CFI=\""+CFI+"\" SecTyp=\""+secType+"\" MatDt=\""+expDate+"\" StrkPx=\""+strikePrice+"\" Sym=\""+symbol+"\"/>";
         output = output + "<OrdQty Qty=\""+quantity+"\"/>";
         output = output + "</Order>";
         output = output + "</FIXML>";
--- app/src/main/java/com/chariotinstruments/markets/PhaseOneTradeControl.java
+++ app/src/main/java/com/chariotinstruments/markets/PhaseOneTradeControl.java
@@ -1,19 +1,29 @@
 package com.chariotinstruments.markets;
 
+import android.app.Activity;
+
 import java.util.ArrayList;
 
 /**
  * Created by user on 3/12/16.
  */
-public class PhaseOneTradeControl implements ParseOptionStrikePrice.ParseOptionStrikePriceAsyncListener, ParseOptionExpirations.ParseOptionExpirationsAsyncListener{
+public class PhaseOneTradeControl implements ParseOptionStrikePrice.ParseOptionStrikePriceAsyncListener, ParseOptionExpirations.ParseOptionExpirationsAsyncListener, ParseOptionOrderPreview.ParseOptionOrderPreviewListener{
     private boolean isOpeningTrade;
     private boolean isCall;
     private ArrayList<Double> strikeList;
     private String formattedExpiration;
+    private Activity uiActivity;
+    private String symbol;
+    private String expiration;
+    private Double strikePrice;
+    private Double curPrice;
 
-    public PhaseOneTradeControl(boolean isOpeningTrade, boolean isCall){
+    public PhaseOneTradeControl(boolean isOpeningTrade, boolean isCall, Activity activity, String symbol, Double curPrice){
         this.isOpeningTrade = isOpeningTrade;
         this.isCall = isCall;
+        this.uiActivity = activity;
+        this.symbol = symbol;
+        this.curPrice = curPrice;
     }
 
     public void setStrikeList(ArrayList<Double> list){
@@ -24,12 +34,77 @@ public class PhaseOneTradeControl implements ParseOptionStrikePrice.ParseOptionS
         formattedExpiration = formattedExpirationIn;
     }
 
-    @Override
+
+    protected String executeTrade(){
+        String ret = "";
+        FixmlModel fixml;
+
+        new ParseOptionExpirations(uiActivity, this, symbol);
+        new ParseOptionStrikePrice(uiActivity, this, symbol);
+
+        fixml = buildFixml();
+
+        new ParseOptionOrderPreview(uiActivity, this, fixml);
+
+        return ret;
+    }
+
+    public void onParseOptionOrderPreviewComplete(String response){
+        
+    }
+
     public void onParseOptionExpirationsComplete(String expiration) {
-        //do something with the expiration
+        this.expiration = expiration;
     }
 
     public void onParseOptionStrikePriceComplete(ArrayList<Double> strikeList){
-        //do something with the strike list
+        if(isCall){
+            double retStrikePrice = 0.0;
+            int index = -1;
+            retStrikePrice = Math.ceil(curPrice);
+            retStrikePrice += 1.0;
+            index = strikeList.indexOf(retStrikePrice);
+
+            if(index > -1){
+                this.strikePrice = retStrikePrice;
+            }
+        }else{
+            double retStrikePrice = 0.0;
+            int index = -1;
+            retStrikePrice = Math.floor(curPrice);
+            retStrikePrice -= 1.0;
+            index = strikeList.indexOf(retStrikePrice);
+
+            if(index > -1){
+                this.strikePrice = retStrikePrice;
+            }
+        }
+
     }
+
+    private FixmlModel buildFixml(){
+        FixmlModel fixml = new FixmlModel(false);
+        int orderSide = -1;
+        String posEffect = "";
+        String CFI = "";
+
+        if(isOpeningTrade){
+            orderSide = 1;
+            posEffect = "O";
+        }else{
+            orderSide = 2;
+            posEffect = "C";
+        }
+
+        if(isCall){
+            CFI = "OC";
+        }else{
+            CFI = "OP";
+        }
+
+        fixml.createFIXMLObject(false, 1, orderSide, posEffect, CFI, "OPT", this.expiration, this.strikePrice, this.symbol, 10);
+
+        return fixml;
+    }
+
 }
