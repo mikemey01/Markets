58950e078a8c71c0c54f6721adb0bee284dfe068
mikemey01 <mikemey01@gmail.com>
2016-03-29 10:10:53 -0700

    adding p2 indicator control

---
+++ app/src/main/java/com/chariotinstruments/markets/PhaseTwoIndicatorControl.java
@@ -0,0 +1,11 @@
+package com.chariotinstruments.markets;
+
+/**
+ * Created by user on 3/29/16.
+ */
+public class PhaseTwoIndicatorControl {
+
+    public PhaseTwoIndicatorControl(){
+        
+    }
+}
