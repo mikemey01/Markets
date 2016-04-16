583b8f17064640c475d72a46d94eba1f8b995981
mikemey01 <mikemey01@gmail.com>
2016-04-16 13:21:56 -0600

    adding apiKeys template class

---
+++ app/src/main/java/com/chariotinstruments/markets/APIKeysTemplate.java
@@ -0,0 +1,17 @@
+package com.chariotinstruments.markets;
+
+/**
+ * This is a template class. Create a new class named APIKeys,
+ * copy this class as is and populate each four TradeKing keys and your account number.
+ */
+public class APIKeysTemplate {
+    public static final String CONSUMER_KEY = "";
+    public static final String CONSUMER_SECRET = "";
+    public static final String OAUTH_TOKEN = "";
+    public static final String OAUTH_TOKEN_SECRET = "";
+    public static final String ACCOUNT_NUMBER = "";
+
+    public APIKeysTemplate(){
+
+    }
+}
